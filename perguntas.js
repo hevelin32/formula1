b745864c02bd218dd017b7d9dcc8ee08d6753011
hevelin32 <133216434+hevelin32@users.noreply.github.com>
2024-11-05 08:09:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,78 @@
+criaCartao(
+    'Fórmula 1' 
+    'Qual foi o primeiro brasileiro a vencer na fórmula 1?',
+    'Emerson Fittipaldi, foi o primeiro brasileiro a cruzar a linha de chegada, conquistando o GP de Watkins Glen em 1970'
+)
+
+criaCartao(
+    'Fórmula 1',
+    'Qual pista todo corredores de formula 1 tem medo?',
+    'Spa-Francorchamps, Bélgica'
+)
+
+criaCartao(
+    'fórmula 1',
+    'Quais são os pilotos com mais título na história da fórmula 1?',
+    'Lewis Hamilton e Michael Schumacher'
+)
+
+criaCartao(
+    'Fórmula 1',
+    'Quem é o piloto mais jovem a vencer uma corrida de fórmula 1?',
+    'Max Vers'
+)
+
+criaCartao(
+    'Fórmula 1',
+    'Qual foi o acidente mais grave da fórmula 1 nos ultimos anos?',
+    'O acidente de Romain Grosjean no grande premio'
+)
+criaCartao(
+    'Matemática',
+    'O que diz o teorema de pitágoras?',
+    'Em todo triângulo retângulo a soma dos quadrados dos catetos é igual ao quadrado da hipotenusa'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
+criaCartao(
+    'Matemática',
+    'O que diz o teorema de pitágoras?',
+    'Em todo triângulo retângulo a soma dos quadrados dos catetos é igual ao quadrado da hipotenusa'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
+criaCartao(
+    'Matemática',
+    'O que diz o teorema de pitágoras?',
+    'Em todo triângulo retângulo a soma dos quadrados dos catetos é igual ao quadrado da hipotenusa'
+)
